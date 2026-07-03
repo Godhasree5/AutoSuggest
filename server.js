529434187ae7207a355b890dc85167790ce9806e
Medi Godhasree <godhasreem@gmail.com>
2026-07-03 13:42:16 +0530

    commit it

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const app=express()
- const PORT=3002;
+ const PORT=process.env.PORT||3002;  //process.env.PORT is used when we are unable to get port it will help us for getting the free port 
  //web server:
  app.use(express.static("frontend"))
  app.listen(PORT,function()
